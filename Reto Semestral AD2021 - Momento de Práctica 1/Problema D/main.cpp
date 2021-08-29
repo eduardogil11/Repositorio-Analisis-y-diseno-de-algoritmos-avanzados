5fae1dec43c156dc3f39a486de4a9e88db971576
eduardogil11 <70300067+eduardogil11@users.noreply.github.com>
2021-08-29 11:41:37 -0500

    Problema D

--- Reto Semestral AD2021 - Momento de Práctica 1/Problema D/main.cpp
+++ Reto Semestral AD2021 - Momento de Práctica 1/Problema D/main.cpp
@@ -1,58 +1,69 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <queue>
+#include <map>
 
 using namespace std;
 
-struct node {
-    node* der;
-    node* izq;
-    int x;
-    int y;    
+class node {
+    public:
+    node* der; 
+    node* izq; 
+    int x; 
+    node(int y) {
+        x = y;
+        der = NULL;
+        izq = NULL;
+    }    
 };
 
-node * nuevoNode(int a) {
-    node* Node = new node();
-    Node -> y = a;
-    Node -> izq = Node -> der = NULL;
-    return Node;
-}
-
-void topView(node * root) {
-    if(root == NULL)
-       return;
-    queue <node* > m;
-    map <int, int> n;
-    int x = 0;
-    root -> x = x;
-    m.push(root);
-
-    while(m.size()) {
-        x = root -> x;
-
-        if(n.count(x) == 0)
-           n[x] = root -> y;
-        if(root -> der) {
-           root -> der-> x = x + 1;
-           m.push(root -> der);
+class nuevoNode{
+    public:
+    node* insert(node* root, int x) {
+        if(root == NULL) {
+            return new node(x);
         }
-        if(root -> izq) {
-           root -> izq -> x = x - 1;
-           m.push(root -> izq);
+        else {
+            node* n;
+            if(root -> x <= x) {
+                n = insert(root -> der, x);
+                root -> der = n;
+            }
+            else {
+                n = insert(root -> izq, x);
+                root -> izq = n;
+            }
+            return root;
         }
-        m.pop();
-        root = m.front(); 
     }
-    for(auto i = n.begin(); i != n.end(); i++) {
-        cout << i->second << " ";
-    }
-}
+    void topView(node * root);
+};
 
-int main() {
-    node* root = nuevoNode(1);
-    root -> der = nuevoNode(2);
-    root ->der->der = nuevoNode(5);
-    root->der->der -> izq = nuevoNode(3);
-    root ->der->der -> der = nuevoNode(6);
-    root->der->der -> izq -> der = nuevoNode(4);
-    topView(root);
+void nuevoNode::topView(node * root) {
+    queue<pair<int, node* >> m;
+    m.push(make_pair(0, root));
+    map <int, node*> ans;
+    for(auto i = m.front(); ! m.empty(); m.pop(), i = m.front()) {
+        if(! i.second) continue;
+        ans.insert(i);
+        m.push(make_pair(i.first + 1, i.second -> der));
+        m.push(make_pair(i.first - 1, i.second -> izq));
+        }
+        for(auto a:ans) 
+        cout <<a.second -> x << " ";
+    }
+
+int main(int argc, char * argv[]) {
+    nuevoNode BTS;
+    node* root = NULL;
+    int x;
+    int z;
+    cin >> z;
+
+    for(int i = 0; i < z; i++) {
+        cin >> x;
+        root = BTS.insert(root, x);
+    }
+    BTS.topView(root);
     return 0;
 }
